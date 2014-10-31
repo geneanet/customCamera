65f7380332be69244652b16257f7834a0fb6a84c
Christophe Boucaut <cboucaut@geneanet.org>
2014-10-31 17:02:45 +0100

    Mise en place de la structure pour la partie js.

---
+++ www/customCamera.js
@@ -0,0 +1,18 @@
+/**
+ * Permet de faire l'interface entre phonegap et l'application customcamera en java.
+ */
+
+'use strict';
+
+var exec = require('cordova/exec');
+
+// constructor.
+var customCameraExport = function() {
+};
+
+// add method.
+customCameraExport.prototype.jesaispasquoionverra = function() {
+    
+};
+
+module.exports = new customCameraExport();
