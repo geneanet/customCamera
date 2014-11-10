79c8b851b8740af542ec2e65b14a75ff7d5ceb2a
Thomas Boy <thomas.boy@geneanet.org>
2014-11-10 09:48:41 +0100

    Toute la semaine dernière -> miniature, opacite, orientation ...

--- src/android/src/org/geneanet/customcamera/CameraView.java
+++ src/android/src/org/geneanet/customcamera/CameraView.java
@@ -1,45 +1,56 @@
 package org.geneanet.customcamera;
 
+import java.util.List;
+import java.lang.Math;
+
 import org.geneanet.customcamera.utils.CameraPreview;
 import org.geneanet.customcamera.utils.CustomCamera;
 import org.geneanet.testcustomcamera.R;
 
 import android.app.Activity;
-import android.content.res.Configuration;
-import android.graphics.Matrix;
-import android.graphics.Point;
+import android.content.pm.ActivityInfo;
+
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.Display;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class CameraView extends Activity {
 	
-	private Camera mCamera;
+    private float mDist;
+    private Camera mCamera;
+    private int modeMiniature = 0;
     private CameraPreview mPreview;
+    public static final int MEDIA_TYPE_IMAGE = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		//Remove title bar
+		/* Remove title bar */
 	    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-	    //Remove notification bar
+	    
+	    /* Remove notification bar */
 	    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
 		setContentView(R.layout.activity_camera_view);
 		
-		// Récupère les infos sur le device.
+		/* Get informations about the device */
 		Display display = getWindowManager().getDefaultDisplay();
 		
-		// récupère l'objet gérant la camera puis l'oriente en fonction de l'écran.
+		/* Get object which use the camera and orient it in function of the screen */
 		mCamera = CustomCamera.getCameraInstance();
+		
 		switch (display.getRotation()) {
 			case CustomCamera.LANDSCAPE:
 				mCamera.setDisplayOrientation(0);
@@ -55,9 +66,180 @@ public class CameraView extends Activity {
 				break;
 		}
 		
-		// On assigne le rendu à la vue.
+		/* Assign the render to the view */
 		mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+        
+        /* The opacity bar */
+        SeekBar switchOpacity = (SeekBar) findViewById(R.id.switchOpacity);
+        
+        switchOpacity.setOnSeekBarChangeListener(
+        		new OnSeekBarChangeListener() {
+        			int progress = 0;
+        		@Override
+        		public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+        			progress = progresValue;
+        			ImageView imageView = (ImageView) findViewById(R.id.normal);
+        			switch(progress){
+        			case 0 : 
+        				imageView.setAlpha((float)0.2);
+        				break;
+        			case 1 :
+        				imageView.setAlpha((float)0.3);
+        				break;
+        			case 2 :
+        				imageView.setAlpha((float)0.4);
+        				break;
+        			case 3 :
+        				imageView.setAlpha((float)0.5);
+        				break;
+        			case 4 :
+        				imageView.setAlpha((float)0.6);
+        				break;
+        			case 5 :
+        				imageView.setAlpha((float)0.7);
+        				break;
+        			case 6 :
+        				imageView.setAlpha((float)0.8);
+        				break;
+        			case 7 :
+        				imageView.setAlpha((float)0.9);
+        				break;
+        			}
+        		}
+
+        		@Override
+        		public void onStartTrackingTouch(SeekBar seekBar) {}
+
+        		@Override
+        		public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+ 	}
+	
+	/*******************************************************/
+	/** BLOCK THE ROTATION IN FUNCTION OF THE POSTAL CARD **/
+	/*******************************************************/
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus){
+		ImageView imageView = (ImageView) findViewById(R.id.normal);
+	    int widthImage=imageView.getWidth();
+	    int heightImage=imageView.getHeight();
+	    
+	    System.out.println("Largeur : "+widthImage);
+	    System.out.println("Hauteur : "+heightImage);
+		
+	    if(heightImage < widthImage || imageView.getRotation() != 0){
+	    	this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+	    	imageView.setRotation(0);
+	    }
+	    else{
+	    	this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+	    }
+	}
+	
+	/******************/
+	/** FOR THE ZOOM **/
+	/******************/
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+	    Camera.Parameters params = mCamera.getParameters();
+	    int action = event.getAction();
+ 
+	    if (event.getPointerCount() > 1) {
+	        // If we touch with more than one finger
+	        if (action == MotionEvent.ACTION_POINTER_UP) {
+	           mDist = getFingerSpacing(event);
+	        } else if (action == MotionEvent.ACTION_MOVE && params.isZoomSupported()) {
+	            mCamera.cancelAutoFocus();
+				handleZoom(event, params, mDist);
+	        }
+	    } else {
+	        // If we touch with one finger -> auto-focus
+	        if (action == MotionEvent.ACTION_UP) {
+	            handleFocus(event, params);
+	        }
+	    }
+	    return true;
+	}
+
+	/******************/
+	/** GERE LE ZOOM **/
+	/******************/
+	private void handleZoom(MotionEvent event, Camera.Parameters params, float mDist) {
+	    int maxZoom = params.getMaxZoom();
+	    int zoom = params.getZoom();
+	    float newDist = getFingerSpacing(event);
+	    
+	    if (newDist > mDist) {
+	        //zoom in
+	        if (zoom < maxZoom/2)
+	            zoom+=2;
+	    } else if (newDist < mDist) {
+	    	//zoom out
+		    if (zoom > 0)
+		        zoom-=2;
+	    	}
+		mDist = newDist;
+		params.setZoom(zoom);
+		mCamera.setParameters(params);
+	}
+
+	/*******************/
+	/** GERE LE FOCUS **/
+	/*******************/
+	public void handleFocus(MotionEvent event, Camera.Parameters params) {
+	    List<String> supportedFocusModes = params.getSupportedFocusModes();
+	    if (supportedFocusModes != null && supportedFocusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+	        mCamera.autoFocus(new Camera.AutoFocusCallback() {
+	            @Override
+	            public void onAutoFocus(boolean b, Camera camera) {}
+	        });
+	    }
+	}
+
+	/*******************************************************/
+	/** DETERMINE L'ESPACE ENTRE LES DEUX PREMIERS DOIGTS **/
+	/*******************************************************/
+	private float getFingerSpacing(MotionEvent event) {
+	    float x = event.getX(0) - event.getX(1);
+	    float y = event.getY(0) - event.getY(1);
+	    return (float) Math.sqrt(x * x + y * y);
+	}
+	
+	/***************************/
+	/** AFFICHER LA MINIATURE **/
+	/***************************/
+	public void showMiniature(View view){
+		ImageView imageView = (ImageView) findViewById(R.id.normal);
+		Button miniature = (Button) findViewById(R.id.Miniature);
+		
+		if(modeMiniature == 0){
+			FrameLayout.LayoutParams paramsMiniature = new FrameLayout.LayoutParams(imageView.getWidth()/4, imageView.getHeight()/4);
+			
+			paramsMiniature.gravity=Gravity.BOTTOM;
+			imageView.setAlpha(imageView.getAlpha());
+			
+			modeMiniature = 1;
+			
+			imageView.setLayoutParams(paramsMiniature);
+			
+			miniature.setEnabled(false);
+			miniature.setVisibility(View.INVISIBLE);
+			imageView.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                	ImageView imageView = (ImageView) findViewById(R.id.normal);
+                	Button miniature = (Button) findViewById(R.id.Miniature);
+                	LayoutParams paramsReagrandissement = (LayoutParams) imageView.getLayoutParams();
+        			paramsReagrandissement.width = -1;
+        			paramsReagrandissement.height = -1;
+        			imageView.setAlpha(imageView.getAlpha());
+        			modeMiniature = 0;
+        			imageView.setLayoutParams(paramsReagrandissement);
+        			miniature.setVisibility(View.VISIBLE);
+        			miniature.setEnabled(true);
+                }
+			});
+		}
 	}
 }
--- src/android/src/org/geneanet/customcamera/MainActivity.java
+++ src/android/src/org/geneanet/customcamera/MainActivity.java
@@ -6,14 +6,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-
 
 public class MainActivity extends Activity {
 
@@ -29,7 +25,6 @@ public class MainActivity extends Activity {
     
     public void startCamera(View view) {
     	if(this.checkCameraHardware(this)){
-    		System.out.println("test");
     		Intent intent = new Intent(this, CameraView.class);
             startActivity(intent);
             finish();
